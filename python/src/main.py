b0646897e5e2a49992c63686639699bc4ed0024b
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-12-23 17:55:48 -0600

    Simple movement without control

--- python/src/main.py
+++ python/src/main.py
@@ -5,8 +5,6 @@ Servo servo10;
 Servo servo11;
 Servo servo12;
 //Servo servo13;
-const int buttonPin = 7;
-int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup()
 {
@@ -15,33 +13,29 @@ void setup()
   servo11.attach(11);
   servo12.attach(12);
   //servo13.attach(13);
-  pinMode(buttonPin, INPUT);
 }
 
 void loop()
 {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
-    servo9.write(110);
-    delay(1000);
-    servo10.write(80);
-    delay(1000);
+  servo9.write(110);
+  delay(1000);
+  servo10.write(80);
+  delay(1000);
 
-    servo11.write(45);
-    delay(1000);
+  servo11.write(45);
+  delay(1000);
 
-    servo12.write(80);
-    delay(1000);
+  servo12.write(80);
+  delay(1000);
 
-    servo11.write(90);
-    delay(1000);
-    servo9.write(0);
-    delay(1000);
+  servo11.write(90);
+  delay(1000);
+  servo9.write(0);
+  delay(1000);
 
-    servo11.write(45);
-    delay(1000);
+  servo11.write(45);
+  delay(1000);
 
-    servo12.write(45);
-  } else{
-  }
+  servo12.write(45);
+  delay(1000);
 }
