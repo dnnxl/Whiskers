431d17c087f91e8b1917d645e2b38a5438bfaca9
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-12-23 15:53:20 -0600

    Update main.py

--- python/src/main.py
+++ python/src/main.py
@@ -1,21 +1,47 @@
-from time import sleep
-import RPi.GPIO as GPIO
-import Servo
+#include <Servo.h>
 
-GPIO.setmode(GPIO.BOARD)
+Servo servo9;
+Servo servo10;
+Servo servo11;
+Servo servo12;
+//Servo servo13;
+const int buttonPin = 7;
+int buttonState = 0;         // variable for reading the pushbutton status
 
-servo11 = Servo(11)
-servo13 = Servo(13)
-servo15 = Servo(15)
-servo31 = Servo(31)
-servo29 = Servo(29)
+void setup()
+{
+  servo9.attach(9);
+  servo10.attach(10);
+  servo11.attach(11);
+  servo12.attach(12);
+  //servo13.attach(13);
+  pinMode(buttonPin, INPUT);
+}
 
-while True:
-    servo11.move(110)
-    servo13.move(80)
-    servo15.move(45)
-    servo29.move(80)
-    servo15.move(90)
-    servo11.move(0)
-    servo15.move(45)
-    servo29.move(45)
+void loop()
+{
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+    servo9.write(110);
+    delay(1000);
+    servo10.write(80);
+    delay(1000);
+
+    servo11.write(45);
+    delay(1000);
+
+    servo12.write(80);
+    delay(1000);
+
+    servo11.write(90);
+    delay(1000);
+    servo9.write(0);
+    delay(1000);
+
+    servo11.write(45);
+    delay(1000);
+
+    servo12.write(45);
+  } else{
+  }
+}
