c2a6dfa27223f3b631ab7a9cb104749aa474efec
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-12-22 21:29:44 -0600

    Update main.py

--- python/src/main.py
+++ python/src/main.py
@@ -1,4 +1,21 @@
 from time import sleep
-import RPi.GPIO
+import RPi.GPIO as GPIO
+import Servo
 
-if __init__ == "main":
+GPIO.setmode(GPIO.BOARD)
+
+servo11 = Servo(11)
+servo13 = Servo(13)
+servo15 = Servo(15)
+servo31 = Servo(31)
+servo29 = Servo(29)
+
+while True:
+    servo11.move(110)
+    servo13.move(80)
+    servo15.move(45)
+    servo29.move(80)
+    servo15.move(90)
+    servo11.move(0)
+    servo15.move(45)
+    servo29.move(45)
