14cf439c640cb8b81ddc94ecaeb79c6ee8700fa2
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2019-12-22 22:02:20 -0600

    Update Whiskers.ino

--- Whiskers/Whiskers.ino
+++ Whiskers/Whiskers.ino
@@ -15,40 +15,32 @@
 */
 #include <Servo.h>
 
-Servo servo1;
+Servo servo9;
+Servo servo10;
+Servo servo11;
+Servo servo12;
+Servo servo13;
 
 int angulo = 0;
-int Eje_X = A1;
-int Eje_Y = A2;
-int boton = 4;
-const int buttonPin = 2;
-float value;
-int buttonState = 0;    
-int once;
 
 void setup()
 {
-  servo1.attach(6) ;
-  pinMode(boton, INPUT_PULLUP) ;  
-  pinMode(buttonPin, INPUT);
-  value = 0;
+  servo9.attach(9);
+  servo10.attach(10);
+  servo11.attach(11);
+  servo12.attach(12);
+  servo13.attach(13); 
 }
 
 void loop()
 {
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
-    value = value;
-    once = 1;
-  }
-  if (once == 1)
-  {
-    value = value;
-  }
-  else
-  {
-    value = analogRead(A1);
-  }
-  angulo  = map( value, 0, 1024, 0, 180);
-  servo1.write(angulo);;
+   servo9.write(110);
+   servo10.write(80);
+   servo11.write(45);
+   servo12.write(80);
+   servo11.write(90);
+     
+   servo9.write(0);
+   servo11.write(45);
+   servo12.write(45);
 }
